8d1ad18f21f8f1eb4c271a9a845c0dac4baea1e9
mlormois <mlormois@student.42.fr>
2022-12-10 11:25:05 +0100

    refont du ex09 min

--- ex09/srcs/main.cpp
+++ ex09/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlormois <mlormois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 20:43:14 by mlormois          #+#    #+#             */
-/*   Updated: 2022/12/05 17:54:54 by mlormois         ###   ########.fr       */
+/*   Updated: 2022/12/10 11:15:08 by mlormois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int add_unique_value( Set const & list, int const & max )
 
 void set_print( Set data )
 {
-	std::cout << "{";
+	std::cout << "[";
 	for ( size_t i = 0; i < data.size(); i++ )
 	{
 		std::cout << data[i];
 		if ( i < data.size() - 1 )
 			std::cout << ", ";
 	}
-	std::cout << "}" << std::endl;
+	std::cout << "]" << std::endl;
 }
 
 void sets_print( Sets data )
@@ -56,20 +56,32 @@ Sets make_sets( int number, int range, int max )
 	return tab_set;
 }
 
-int main( void )
+int main( int ac, char **av )
 {
 	srand( time( NULL ) );
-	Sets tab_set = make_sets( 6 , 10 , 20);
+	
+	if (ac != 2)
+		return 1;
+
+	Sets tab_set_empty = make_sets( 2 , 0 , 10);
+	Sets tab_set = make_sets( 2 , 4 , 10);
+
 	if ( tab_set.size() == 0 )
 		return (std::cout << "Error: make_sets invalid\n", 1);
 	sets_print( tab_set );
+	std::cout << std::endl;
+	// sets_print( tab_set_empty );
+	// std::cout << std::endl;
 
 	try {
-		set_print( eval_set( "AB&!", tab_set) );
+		set_print( eval_set( av[1], tab_set) );
+		// std::cout << std::endl;
+		// set_print( eval_set( av[1], tab_set_empty) );
 	} catch(const std::invalid_argument& ia)
 	{
 		std::cerr << "Invalide Argument: expression " << ia.what() << std::endl;
 		return 1;
 	}
+
 	return 0;
 }
